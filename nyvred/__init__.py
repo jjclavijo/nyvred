06f731991c91a123beac106419198c90e4dd25d8
jjclavijo <jjclavijo@gmail.com>
2021-07-27 12:02:43 -0300

    fix typo

--- nyvred/__init__.py
+++ nyvred/__init__.py
@@ -4,6 +4,6 @@ Esqueleto básico para el calculo de compensación de nivelaciones.
 Por Javier Clavijo
 """
 
-from .nivelacion import Lecturas,Red
+from .nivelacion import Lectura,Red
 
 __version__ = '0.0.1'
